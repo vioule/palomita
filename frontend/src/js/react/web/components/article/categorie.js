5c1fd61c50372049f6841028a09942e9689a0c8a
Vincent <vcano.fr@outlook.com>
2020-02-27 07:47:06 +0100

    CI (article) : ajout elements - title - date - social

---
+++ frontend/src/js/react/web/components/article/categorie.js
@@ -0,0 +1,4 @@
+const React = require("react");
+export default (props) => (
+<h2 className="categorie">{props.categorie}</h2>
+);
--- frontend/src/js/react/web/components/article/index.js
+++ frontend/src/js/react/web/components/article/index.js
@@ -2,18 +2,25 @@ const React = require("react");
 import { connect } from 'react-redux';
 import { setArticle } from '../../actions/article';
 import Title from './title';
+import Categorie from './categorie';
 import Content from './content';
 import Socials from '../social';
+import Datetime from '../card/date';
 
 export class Article extends React.Component {
   render(){
     return (
     <div className="article">
-      <Title title={this.props.article.title}/>
-      <Content content={this.props.article.content}>
-        <Socials share={true}/>
-      </Content>
-      {this.props.article.comments.map(com=><p key={com._id}>{com.content}</p>)}
+      <Categorie categorie={this.props.article.categorie}/>
+      <div className="background">
+        <Socials/>
+        <Title title={this.props.article.title}/>
+        <Content content={this.props.article.content}>
+          <Socials share={true}/>
+        </Content>
+        {this.props.article.comments.map(com=><p key={com._id}>{com.content}</p>)}
+      </div>
+      <Datetime date={new Date(this.props.article.date).toLocaleDateString().replace(/\//g,".")}/>
     </div>
     )
   };
